80b105ddeaedc799cfcb1cab6595f0ae933984c1
Susie Gordon <susieyoungchoi@gmail.com>
2023-05-25 12:50:48 -0400

    added CSS

--- js/app.js
+++ js/app.js
@@ -7,20 +7,24 @@ $searchForm.on("submit", event => {
     const formData = new FormData(event.target)
     console.log(formData);
 
-    // input can use upper or lowercase - won't affect search
-    const makeupInput = formData.get("makeupInput").toLowerCase();
+    // input can use upper or lowercase
+    const searchInput = formData.get("search input").toLowerCase();
 
     // link makeupInput search to url
-    const url = `https://makeup-api.herokuapp.com/api/v1/products.json${makeupInput}`
+    const url = `https://makeup-api.herokuapp.com/api/v1/products.json${searchInput}`
 
     $.ajax('https://makeup-api.herokuapp.com/api/v1/products.json')
     .then((data) => {
         console.log(data);
-        // retrieve brand,category, img_link
-  
+        console.log(data[0].brand)
+        // retrieve brand, name, description, category & img_link
+        // use .filter()
+        // data.filter((item) => {})
+        
     
     })  
 })
+
 
 
 
